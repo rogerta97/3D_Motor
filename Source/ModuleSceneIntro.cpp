2f8308777bda8fd8bce9e50c04a1e5d449e14146
rogerta97 <rogerteello97@gmail.com>
2017-09-17 17:55:16 +0200

    Adding base code

---
+++ Source/ModuleSceneIntro.cpp
@@ -0,0 +1,60 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+
+
+#include <stdlib.h>
+#include <time.h>
+
+#define RADIUS 44
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	srand(time(NULL));
+
+	//App->audio->PlayMusic("F-Zero_-_Mute_City.ogg");
+	
+	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
+	App->camera->LookAt(vec3(0, 0, 0));
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+	/*Plane p(0, 1, 0, 0);
+	p.axis = true;
+	p.Render();*/
+
+	for (p2List_item<Cylinder>* aux = Cylinders.getFirst(); aux != nullptr; aux = aux->next) {
+		aux->data.Render();
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
+}
