70f0cf157aff193a3d32c5b34866109c39e1c26f
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2017-10-10 13:13:17 +0200

    Assimp addition

---
+++ Source/ModuleFBXLoader.h
@@ -0,0 +1,16 @@
+#ifndef _FBX_LOADER_
+#define _FBX_LOADER_
+
+#include "Assimp/include/cimport.h"
+#include "Assimp/include/scene.h"
+#include "Assimp/include/postprocess.h"
+#include "Assimp/include/cfileio.h"
+
+#include "mmgr/mmgr.h"
+#pragma comment (lib, "Assimp/libx86/assimp.lib")
+
+class ModuleFBXLoader : public Module
+{
+
+};
+#endif // !_FBX_LOADER_
