6811900b0de8fe6ee6f16f93016127073ec73eab
Roger Tello Araez <rogerta@TTC.CAT>
2017-10-10 14:25:34 +0200

    Add FBX loader

---
+++ Source/ModuleFBXLoader.cpp
@@ -0,0 +1,80 @@
+#include "ModuleFBXLoader.h"
+
+
+ModuleFBXLoader::ModuleFBXLoader()
+{
+}
+
+ModuleFBXLoader::~ModuleFBXLoader()
+{
+}
+
+bool ModuleFBXLoader::Start()
+{
+
+	struct aiLogStream stream;
+	stream = aiGetPredefinedLogStream(aiDefaultLogStream_DEBUGGER, nullptr);
+	aiAttachLogStream(&stream);
+	return true;
+}
+
+update_status ModuleFBXLoader::Update(float dt)
+{
+
+	return update_status();
+}
+
+bool ModuleFBXLoader::CleanUp()
+{
+
+	aiDetachAllLogStreams(); 
+
+	return true;
+}
+
+bool ModuleFBXLoader::LoadFBX(const char* full_path)
+{
+	bool ret = true; 
+
+	const aiScene* scene = aiImportFile(full_path, aiProcessPreset_TargetRealtime_MaxQuality);
+
+	if (scene != nullptr && scene->HasMeshes())
+	{
+		// Use scene->mNumMeshes to iterate on scene->mMeshes array
+		for (int i = 0; i < scene->mNumMeshes; i++)
+		{
+			aiMesh* new_mesh = scene->mMeshes[i];
+
+			curr_mesh.num_vertices = new_mesh->mNumVertices;
+			curr_mesh.vertices = new float[curr_mesh.num_vertices * 3];
+			memcpy(curr_mesh.vertices, new_mesh->mVertices, sizeof(float) * curr_mesh.num_vertices * 3);
+			LOG("New mesh with %d vertices", curr_mesh.num_vertices);
+
+			if (new_mesh->HasFaces())
+			{
+				curr_mesh.num_indices = new_mesh->mNumFaces * 3;
+				curr_mesh.indices = new uint[curr_mesh.num_indices]; // assume each face is a triangle
+
+				for (uint i = 0; i < new_mesh->mNumFaces; ++i)
+				{
+					if (new_mesh->mFaces[i].mNumIndices != 3)
+						LOG("WARNING, geometry face with != 3 indices!");
+
+					else
+						memcpy(&curr_mesh.indices[i * 3], new_mesh->mFaces[i].mIndices, 3 * sizeof(uint));
+				}
+
+				aiReleaseImport(scene);
+			}
+		}
+	}
+	else
+	{
+		LOG("Error loading scene %s", full_path);
+		ret = false; 
+	}
+		
+
+	return ret; 
+}
+
--- Source/ModuleFBXLoader.h
+++ Source/ModuleFBXLoader.h
@@ -6,11 +6,44 @@
 #include "Assimp/include/postprocess.h"
 #include "Assimp/include/cfileio.h"
 
+#include "Module.h"
+#include <list>
+#include "Globals.h"
+
 #include "mmgr/mmgr.h"
 #pragma comment (lib, "Assimp/libx86/assimp.lib")
 
+struct Mesh
+{
+	float* vertices;
+	int num_vertices;
+
+	uint* indices;
+	int num_indices;
+
+	int vertex_buffer; 
+	int indices_buffer; 
+};
+
 class ModuleFBXLoader : public Module
 {
+public: 
+
+	ModuleFBXLoader(); 
+	~ModuleFBXLoader(); 
+		
+	bool Start();
+	update_status Update(float dt); 
+	bool CleanUp(); 
+
+	bool LoadFBX(const char* path);
+	
+private:
+
+	// Current FBX info
+	Mesh curr_mesh; 
 
 };
+
+
 #endif // !_FBX_LOADER_
